68a1f511fdcd6bce4cb7b23bd0e26b1322509f24
Andrei <andrei.ahibalau@gmail.com>
2023-05-10 00:10:59 +0200

    Initial Commit

---
+++ CountriesList/CountriesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListApp.swift
+//  CountriesList
+//
+//  Created by Andrei Ahibalau on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountriesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
